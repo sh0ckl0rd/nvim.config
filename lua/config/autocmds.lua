621d5f775a3cc1b5383d4f341a42f56a970042b9
Arseniy <arseny.sergeenko@gmail.com>
2026-03-11 02:11:22 +0200

    added folder structure, and first options

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup,
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
---
+++ lua/config/options.lua
@@ -0,0 +1,72 @@
+-- 0=================================================0
+--		     	OPTIONS
+-- 0=================================================0
+
+vim.g.have_nerd_font = true
+vim.opt.termguicolors = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
+
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.softtabstop = 8
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.completeopt = "menuone,noinsert,noselect"
+vim.opt.showmode = false
+vim.opt.pumheight = 10
+vim.opt.conceallevel = 0
+vim.opt.concealcursor = ""
+vim.opt.synmaxcol = 300
+vim.opt.fillchars = { eob = " " }
+
+local undodir = vim.fn.expand("~/.vim/undodir")
+if
+        vim.fn.isdirectory(undodir) == 0
+then
+        vim.fn.mkdir(undodir, "p")
+end
+
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undodir = undodir
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+vim.opt.ttimeoutlen = 0
+vim.opt.autoread = true
+vim.opt.autowrite = false
+
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.backspace = "indent,eol,start"
+vim.opt.autochdir = false
+vim.opt.iskeyword:append("-")
+vim.opt.path:append("**")
+vim.opt.selection = "inclusive"
+vim.opt.mouse = "a"
+-- vim.opt.clipboard:append("unnamedplus")   
+vim.opt.modifiable = true
+vim.opt.encoding = "utf-8"
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wildmenu = true
+vim.opt.wildmode = "longest:full,full"
