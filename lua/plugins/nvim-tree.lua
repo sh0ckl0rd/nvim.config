d619c7861c349d1ac9662f92894ce2bc28a742f1
Arseniy <arseny.sergeenko@gmail.com>
2026-03-11 03:32:05 +0200

    moare plugins!

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,14 @@
+require("nvim-tree").setup({
+        view = {
+		width = 35,
+	},
+	filters = {
+		dotfiles = false,
+	},
+	renderer = {
+		group_empty = true,
+	},      
+})
+vim.keymap.set("n", "<leader>e", function()
+	require("nvim-tree.api").tree.toggle()
+end, { desc = "Toggle NvimTree" })
--- lua/plugins/pack.lua
+++ lua/plugins/pack.lua
@@ -1,8 +1,7 @@
 vim.pack.add({
-        https://github.com/nvim-tree/nvim-tree.lua
+        "https://github.com/nvim-tree/nvim-tree.lua",
+        "https://github.com/ibhagwan/fzf-lua",
+        "https://github.com/nvim-mini/mini.nvim",
+        "https://github.com/lewis6991/gitsigns.nvim",
 })
 
-local function packadd(name)
-        vim.cmd("packadd " ..name)
-end
-packadd("nvim-tree.lua")
